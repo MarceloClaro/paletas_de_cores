a6ef28a55036d58b4fe3435d8fba0755c18e8de3
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-05-18 05:25:48 -0300

    Update paletas_de_cores_perfis_de_solo.py

--- paletas_de_cores_perfis_de_solo.py
+++ paletas_de_cores_perfis_de_solo.py
@@ -1,3 +1,25 @@
+import streamlit as st
+import numpy as np
+import cv2
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def extrair_paleta(imagem, n_cores):
+    pixels = imagem.reshape(-1, 3)
+
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+    _, labels, centers = cv2.kmeans(pixels.astype(float), n_cores, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+    segmented_image = centers[labels.flatten()]
+    segmented_image = segmented_image.reshape(imagem.shape)
+
+    return centers, segmented_image
+
+st.title('Analisador de Paleta de Cores de Imagens de Solo')
+
+n_cores = st.sidebar.slider('Número de cores na paleta', min_value=1, max_value=20, value=5, step=1)
+imagem_up = st.sidebar.file_uploader('Carregar Imagem de Solo', type=['png', 'jpg', 'jpeg'])
+
 if imagem_up is not None:
     imagem = Image.open(imagem_up)
     imagem_cv = np.array(imagem)
