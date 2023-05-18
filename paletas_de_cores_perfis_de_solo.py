5d5edb1de050228d8070c2d129fbf1f5905014dc
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-05-18 20:41:52 -0300

    Update paletas_de_cores_perfis_de_solo.py

--- paletas_de_cores_perfis_de_solo.py
+++ paletas_de_cores_perfis_de_solo.py
@@ -4,6 +4,8 @@ from sklearn.cluster import KMeans
 from sklearn.utils import shuffle
 import cv2
 import streamlit as st
+import ntpath
+
 
 class Canvas():
     """
@@ -16,8 +18,27 @@ class Canvas():
 
     def generate(self):
         """Função principal"""
-        quantified_image, colors = self.quantification(self.src)
-        return quantified_image
+        im_source = self.resize()
+        clean_img = self.cleaning(im_source)
+        quantified_image, colors = self.quantification(clean_img)
+        return quantified_image, colors
+
+    def resize(self):
+        """Redimensiona a imagem para corresponder à largura-alvo e respeitar a proporção da imagem"""
+        (height, width) = self.src.shape[:2]
+        if height > width:  # modo retrato
+            dim = (int(width * self.tar_width / float(height)), self.tar_width)
+        else:
+            dim = (self.tar_width, int(height * self.tar_width / float(width)))
+        return cv2.resize(self.src, dim, interpolation=cv2.INTER_AREA)
+
+    def cleaning(self, picture):
+        """Redução de ruído, operações Morphomat, abertura e depois fechamento"""
+        clean_pic = cv2.fastNlMeansDenoisingColored(picture, None, 10, 10, 7, 21)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8, 8))
+        clean_pic = cv2.morphologyEx(clean_pic, cv2.MORPH_OPEN, kernel, cv2.BORDER_REPLICATE)
+        clean_pic = cv2.morphologyEx(clean_pic, cv2.MORPH_CLOSE, kernel, cv2.BORDER_REPLICATE)
+        return clean_pic
 
     def quantification(self, picture):
         """Retorna a imagem K-mean"""
@@ -35,6 +56,7 @@ class Canvas():
         out = vfunc(np.arange(width*height))
         return np.resize(out, (width, height, codebook.shape[1]))
 
+
 if __name__ == "__main__":
     st.title("Transforme a sua imagem em um canvas")
 
@@ -47,5 +69,5 @@ if __name__ == "__main__":
 
         if st.button('Gerar'):
             canvas = Canvas(image, nb_color)
-            result = canvas.generate()
-            # Aqui você pode fazer o que quiser com a imagem resultante
+            result, colors = canvas.generate()
+            # Aqui você pode fazer o que quiser com a imagem resultante e as cores
