b0ca2a749fa50bf15db6ed80df617e7c184d7368
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-05-19 23:47:03 -0300

    Update paletas_de_cores.py

--- paletas_de_cores.py
+++ paletas_de_cores.py
@@ -1,20 +1,11 @@
-# -*- coding: utf-8 -*-
-"""Script para gerar uma paleta de cores a partir de uma imagem"""
-
-# Importando todas as coisas necessárias para o nosso programa funcionar.
-# Esses são como os blocos de construção que vamos usar para fazer o nosso programa.
-
-import numpy as np  # Esta é uma ferramenta para lidar com listas de números.
-from sklearn.cluster import KMeans  # Essa é uma ferramenta que nos ajuda a encontrar grupos de coisas.
-from sklearn.utils import shuffle  # Isso nos ajuda a misturar coisas.
-import cv2  # Esta é uma ferramenta para trabalhar com imagens.
-import streamlit as st  # Isso é o que nos permite criar a interface do nosso programa.
-from PIL import Image  # Outra ferramenta para trabalhar com imagens.
-import io  # Essa é uma ferramenta que nos ajuda a lidar com arquivos e dados.
-import base64  # Essa é uma ferramenta que nos ajuda a converter dados.
-
-# Aqui estamos criando uma nova ferramenta que chamamos de "Canvas".
-# Isso nos ajuda a lidar com imagens e cores.
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.utils import shuffle
+import cv2
+import streamlit as st
+from PIL import Image
+import io
+import base64
 
 def rgb_to_cmyk(r, g, b):
     if (r == 0) and (g == 0) and (b == 0):
@@ -41,7 +32,7 @@ def calculate_percentage(c, m, y, k):
 
 class Canvas():
     def __init__(self, src, nb_color, pixel_size=4000):
-        self.src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)  # Corrige a ordem dos canais de cor
+        self.src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
         self.nb_color = nb_color
         self.tar_width = pixel_size
         self.colormap = []
@@ -72,7 +63,7 @@ class Canvas():
 
     def resize(self):
         (height, width) = self.src.shape[:2]
-        if height > width:  # modo retrato
+        if height > width:
             dim = (int(width * self.tar_width / float(height)), self.tar_width)
         else:
             dim = (self.tar_width, int(height * self.tar_width / float(width)))
@@ -98,30 +89,25 @@ class Canvas():
         vfunc = lambda x: codebook[labels[x]]
         out = vfunc(np.arange(width * height))
         return np.resize(out, (width, height, codebook.shape[1]))
-    
-# Aqui é onde começamos a construir a interface do nosso programa.
-# Estamos adicionando coisas como texto e botões para as pessoas interagirem.
 
-st.image("clube.png")  # Adiciona a imagem no topo do app
-st.title('Gerador de Paleta de Cores para Pintura por Números ')
+st.image("clube.png")
+st.title('Gerador de Paleta de Cores para Pintura por Números')
 st.subheader("Sketching and concept development")
-
-# Isso é para as pessoas fazerem o upload de uma imagem que elas querem usar.
 
 uploaded_file = st.file_uploader("Escolha uma imagem", type=["jpg", "png"])
 st.write("""
-Esse APP é um programa criado pelo clube de artes plástica para gerar uma paleta de cores numeradas a partir de uma imagem. Ele pode ser útil para um artista plástico na sua pintura de várias maneiras.
-Primeiramente, o aplicativo permite que o artista faça o upload de uma imagem de referência, que pode ser uma foto, uma ilustração ou qualquer imagem que ele deseje usar como base. Isso é útil para um artista visualizar uma cena ou um conceito que deseja pintar.
-Em seguida, o aplicativo utiliza o algoritmo K-means para quantificar as cores presentes na imagem. O número de clusters (cores) é determinado pelo artista através de um controle deslizante. Isso permite que o artista controle a quantidade de cores que deseja extrair da imagem.
+Este aplicativo é um programa criado pelo clube de artes plásticas para gerar uma paleta de cores numeradas a partir de uma imagem. Pode ser útil para artistas plásticos em suas pinturas de várias maneiras.
+Primeiro, o aplicativo permite que o artista faça o upload de uma imagem de referência, que pode ser uma foto, uma ilustração ou qualquer imagem que deseje usar como base. Isso é útil para o artista visualizar uma cena ou um conceito que deseja pintar.
+Em seguida, o aplicativo utiliza o algoritmo K-means para quantificar as cores presentes na imagem. O número de clusters (cores) é determinado pelo artista através de um controle deslizante. Isso permite ao artista controlar a quantidade de cores que deseja extrair da imagem.
 Uma vez gerada a paleta de cores, o aplicativo exibe a imagem resultante, onde cada região da imagem original é substituída pela cor correspondente da paleta. Isso pode ajudar o artista a visualizar como sua pintura ficaria usando essas cores específicas.
 Além disso, o aplicativo também exibe a imagem segmentada, onde cada região da imagem original é preenchida com uma cor sólida correspondente à cor dominante da região. Isso pode ajudar o artista a identificar áreas de destaque ou contrastes na imagem, facilitando o processo de esboço e desenvolvimento de conceitos.
-O aplicativo também fornece a opção de baixar a imagem resultante (pintura numerada a ser pintada)e a imagem segmentada (prévia da tela pintada), permitindo que o artista as salve e as utilize como referência durante o processo de pintura.
-Em resumo, esse aplicativo pode ajudar um artista plástico fornecendo uma paleta de cores baseada em uma imagem de referência e auxiliando no esboço e desenvolvimento de conceitos através da imagem segmentada. Ele permite que o artista experimente diferentes combinações de cores e visualize como sua pintura pode ficar antes mesmo de começar a trabalhar na tela.
+O aplicativo também fornece a opção de baixar a imagem resultante (pintura numerada a ser pintada) e a imagem segmentada (prévia da tela pintada), permitindo que o artista as salve e as utilize como referência durante o processo de pintura.
+Em resumo, este aplicativo pode ajudar um artista plástico fornecendo uma paleta de cores baseada em uma imagem de referência e auxiliando no esboço e desenvolvimento de conceitos através da imagem segmentada. Ele permite ao artista experimentar diferentes combinações de cores e visualizar como sua pintura pode ficar antes mesmo de começar a trabalhar na tela.
 """)
 if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     image = cv2.imdecode(file_bytes, 1)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Corrige a ordem dos canais de cor
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     st.image(image, caption='Imagem Carregada', use_column_width=True)
 
     nb_color = st.slider('Escolha o número de cores', min_value=2, max_value=80, value=5, step=1)
@@ -129,55 +115,47 @@ if uploaded_file is not None:
     if st.button('Gerar'):
         pixel_size = st.slider('Escolha o tamanho do pixel', min_value=500, max_value=8000, value=4000, step=100)
 
-        # Tentativa de leitura dos metadados de resolução (DPI)
         pil_image = Image.open(io.BytesIO(file_bytes))
         if 'dpi' in pil_image.info:
             dpi = pil_image.info['dpi']
             st.write(f'Resolução da imagem: {dpi} DPI')
 
-            # Calcula a dimensão física de um pixel
             cm_per_inch = pixel_size
-            cm_per_pixel = cm_per_inch / dpi[0]  # Supõe-se que a resolução seja a mesma em ambas as direções
+            cm_per_pixel = cm_per_inch / dpi[0]
             st.write(f'Tamanho do pixel: {cm_per_pixel:.4f} centímetros')
 
         canvas = Canvas(image, nb_color, pixel_size)
         result, colors, segmented_image = canvas.generate()
 
-        # Converter imagem segmentada para np.uint8
         segmented_image = (segmented_image * 255).astype(np.uint8)
-        
-        # Agora converta de BGR para RGB
         segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_BGR2RGB)
 
         st.image(result, caption='Imagem Resultante', use_column_width=True)
         st.image(segmented_image, caption='Imagem Segmentada', use_column_width=True)
 
-      
-        # Mostrar paleta de cores
-            for i, color in enumerate(colors):
-                color_block = np.ones((50, 50, 3), np.uint8) * color[::-1]  # Cores em formato BGR
-                st.image(color_block, width=50)
+        for i, color in enumerate(colors):
+            color_block = np.ones((50, 50, 3), np.uint8) * color[::-1]
+            st.image(color_block, width=50)
 
-                # Cálculo das proporções das cores CMYK
-                r, g, b = color
-                c, m, y, k = rgb_to_cmyk(r, g, b)
-                c_percent, m_percent, y_percent, k_percent = calculate_percentage(c, m, y, k)
+            r, g, b = color
+            c, m, y, k = rgb_to_cmyk(r, g, b)
+            c_percent, m_percent, y_percent, k_percent = calculate_percentage(c, m, y, k)
 
-                st.write(f"""
-                A cor {i+1} tem os valores RGB ({int(r)}, {int(g)}, {int(b)}).
-                Ela pode ser convertida para o modelo CMYK da seguinte forma:
+            st.write(f"""
+            A cor {i+1} tem os valores RGB ({int(r)}, {int(g)}, {int(b)}).
+            Ela pode ser convertida para o modelo CMYK da seguinte forma:
 
-                Ciano (C): {c_percent:.2f}%
-                Magenta (M): {m_percent:.2f}%
-                Amarelo (Y): {y_percent:.2f}%
-                Preto (K): {k_percent:.2f}%
+            Ciano (C): {c_percent:.2f}%
+            Magenta (M): {m_percent:.2f}%
+            Amarelo (Y): {y_percent:.2f}%
+            Preto (K): {k_percent:.2f}%
 
-                Percentual de 10 ml para cada cor:
-                Ciano (C): {c_percent * 0.1:.2f} ml
-                Magenta (M): {m_percent * 0.1:.2f} ml
-                Amarelo (Y): {y_percent * 0.1:.2f} ml
-                Preto (K): {k_percent * 0.1:.2f} ml
-                """)
+            Percentual de 10 ml para cada cor:
+            Ciano (C): {c_percent * 0.1:.2f} ml
+            Magenta (M): {m_percent * 0.1:.2f} ml
+            Amarelo (Y): {y_percent * 0.1:.2f} ml
+            Preto (K): {k_percent * 0.1:.2f} ml
+            """)
 
         result_bytes = cv2.imencode('.jpg', result)[1].tobytes()
         st.download_button(
