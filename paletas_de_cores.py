e5d04530122d3018b1d1ca416edeadcbf673b778
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-05-19 22:58:32 -0300

    Update paletas_de_cores.py

--- paletas_de_cores.py
+++ paletas_de_cores.py
@@ -129,6 +129,15 @@ if uploaded_file is not None:
 
         st.image(result, caption='Imagem Resultante', use_column_width=True)
         st.image(segmented_image, caption='Imagem Segmentada', use_column_width=True)
+def rgb_to_cmyk(r, g, b):
+    r = r / 255.0
+    g = g / 255.0
+    b = b / 255.0
+    k = 1 - max(r, g, b)
+    c = (1 - r - k) / (1 - k) if k != 1 else 0
+    m = (1 - g - k) / (1 - k) if k != 1 else 0
+    y = (1 - b - k) / (1 - k) if k != 1 else 0
+    return c, m, y, k
 
         # Mostrar paleta de cores
         for i, color in enumerate(colors):
